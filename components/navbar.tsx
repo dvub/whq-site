47fa136fe51965960dfca002068261e43ae8786a
dvub <84478450+dvub@users.noreply.github.com>
2023-05-29 14:23:49 -0700

    style changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,14 +20,14 @@ export default function Navbar() {
         { name: 'Contact', href: 'contact', current: path === '/contact' },
     ]
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav">
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:#8b8b8b focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
                                         <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -44,8 +44,8 @@ export default function Navbar() {
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                    item.current ? 'bg-gray-900' : 'text-gray-300 hover:bg-gray-700',
+                                                    'rounded-md px-3 py-2 text-sm font-medium '
                                                 )}
                                                 aria-current={item.current ? 'page' : undefined}
                                             >
