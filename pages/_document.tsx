5cfb1295ae9e9e7aa9619e300072e011ac8a37fd
dvub <84478450+dvub@users.noreply.github.com>
2023-05-29 14:23:53 -0700

    add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-        <Head/>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;400&family=Josefin+Sans&display=swap" rel="stylesheet"/>
+
+            </Head>
+            <body>
+              <Main />
+              <NextScript />
+            </body>
+          </Html>
+          )
 }
