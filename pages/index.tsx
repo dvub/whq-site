24bc257a9b53117382b57b7e54fc00b9160de346
dvub <84478450+dvub@users.noreply.github.com>
2023-05-29 19:12:47 -0700

    add new component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
+import MusicNote from '../components/musicnote'
+import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <h1 style={{ marginTop: '10vh' }}>Waking Hours</h1>
       <div>
-        <p style={{maxWidth: '50vw', textAlign: 'center', margin: 'auto', marginTop: '10vh'}}>
+        <p style={{ maxWidth: '50vw', textAlign: 'center', margin: 'auto', marginTop: '10vh' }}>
           We&apos;re the Waking Hours Quintet. We play jazz in Reno, NV. Actually, we play a lot of genres. We just like making cool sounds. If you want to learn more about us, go <Link href={'/about'}>here</Link>.
         </p>
-      </div>  
-
-
-    </main>
+      </div>
+      <div style={{margin: 'auto', width: '100%', textAlign:'center'}}><MusicNote/></div>
+    </div>
   )
 }
