ff1d428214ca2072e529bbfe16268ecf11659a55
dvub <84478450+dvub@users.noreply.github.com>
2023-05-30 10:41:26 -0700

    work on about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,41 @@
 export default function About() {
-    return (
-      <main>
-        ABOUT
-      </main>
-    )
-  }
-  
+  return (
+    <div>
+      <h1 style={{ marginTop: "10vh" }}>About Us</h1>
+      <div className="gradient">
+        <p
+          style={{
+            maxWidth: "50vw",
+            textAlign: "left",
+            margin: "auto",
+            marginTop: "10vh",
+          }}
+        >
+          The Waking Hours Quintet is comprised of five players:
+          <br/>
+          <br/>
+          <ul>
+            <li>
+              Jake Xie - Saxophone
+            </li>
+            <li>
+              Teagan Loranz - Guitar
+            </li>
+            <li>
+              Evan Reese - Bass
+            </li>
+            <li>
+              Jackson - Drums
+            </li>
+            <li>
+              Kaya - Piano
+            </li>
+          </ul>
+          <br/>
+          We&apos;ve been playing with music together for years in one way or another, including school bands and other groups.
+          As a combo, we&apos;ve played together for about a year.
+        </p>
+      </div>
+    </div>
+  );
+}
