b096a057823aa624415c25bdbf80643f94a4dff5
dvub <84478450+dvub@users.noreply.github.com>
2023-05-30 10:41:38 -0700

    style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
 import Link from "next/link";
-import MusicNote from '../components/musicnote'
+import MusicNote from "../components/musicnote";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
     <div>
-      <h1 style={{ marginTop: '10vh' }}>Waking Hours</h1>
-      <div>
-        <p style={{ maxWidth: '50vw', textAlign: 'center', margin: 'auto', marginTop: '10vh' }}>
-          We&apos;re the Waking Hours Quintet. We play jazz in Reno, NV. Actually, we play a lot of genres. We just like making cool sounds. If you want to learn more about us, go <Link href={'/about'}>here</Link>.
+      <h1 style={{ marginTop: "10vh" }}>Waking Hours</h1>
+      <div className="gradient">
+        <p
+          style={{
+            maxWidth: "50vw",
+            textAlign: "left",
+            margin: "auto",
+            marginTop: "10vh",
+          }}
+        >
+          We&apos;re the <b>Waking Hours Quintet.</b>
+          &nbsp;We play jazz in <b>Reno, NV</b>. Actually, we play a lot of
+          genres. We just like making <b>cool sounds</b>. If you want to learn
+          more about us, go&nbsp;
+          <Link href={"/about"}>
+            <u>here</u>
+          </Link>
+          .
         </p>
       </div>
-      <div style={{margin: 'auto', width: '100%', textAlign:'center'}}><MusicNote/></div>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "10vh",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <MusicNote />
+      </div>
     </div>
-  )
+  );
 }
