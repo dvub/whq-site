bc42bd37bd02975d5505223f29c0929cb7e4af36
dvub <84478450+dvub@users.noreply.github.com>
2023-05-31 09:58:40 -0700

    create contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>
+                If you&apos;re interested in hiring us for an event, please reach out to one of these emails:
+                
+            </p>
+        </div>
+    )
+}
