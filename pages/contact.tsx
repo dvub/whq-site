d991f4e30a6ea7d929b2a70c81a153e8699b20e8
dvub <84478450+dvub@users.noreply.github.com>
2023-06-01 10:55:54 -0700

    change contact info

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,16 +4,11 @@ export default function Contact() {
             <h1>Contact Us</h1>
             <p>
                 If you&apos;re interested in hiring us for an event, <b>please reach out to one of these emails</b>. Also, you can use these emails if you would like to contact one of us individually.
+                <br/>
+                &gt; jake@wakinghoursquintet.com
+                
             </p>
-            <ul>
-                <li>
-                    &gt; jake@wakinghoursquintet.com
-                </li>
-                <li>&gt; teagan@wakinghoursquintet.com</li>
-                <li>&gt; evan@wakinghoursquintet.com</li>
-                <li>&gt; jackson@wakinghoursquintet.com</li>
-                <li>&gt; kaya@wakinghoursquintet.com</li>
-            </ul>
+            
             <p>
                 (Please don&apos;t spam any of our emails, thx :D)
             </p>
