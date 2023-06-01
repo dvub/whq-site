0e836cfc3dfbd369e54aff46b86e34674371ed4d
dvub <84478450+dvub@users.noreply.github.com>
2023-05-31 20:22:15 -0700

    finish contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,18 +3,20 @@ export default function Contact() {
         <div>
             <h1>Contact Us</h1>
             <p>
-                If you&apos;re interested in hiring us for an event, please reach out to one of these emails. Also, you can use these emails if you would like to individually contact one of us.
-
+                If you&apos;re interested in hiring us for an event, <b>please reach out to one of these emails</b>. Also, you can use these emails if you would like to contact one of us individually.
             </p>
             <ul>
                 <li>
                     &gt; jake@wakinghoursquintet.com
                 </li>
                 <li>&gt; teagan@wakinghoursquintet.com</li>
-                <li>&gt; teagan@wakinghoursquintet.com</li>
+                <li>&gt; evan@wakinghoursquintet.com</li>
                 <li>&gt; jackson@wakinghoursquintet.com</li>
                 <li>&gt; kaya@wakinghoursquintet.com</li>
             </ul>
+            <p>
+                (Please don&apos;t spam any of our emails, thx :D)
+            </p>
         </div>
     )
 }
