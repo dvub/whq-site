8b75b27a9ffe47b854df9dc004c019351174b630
dvub <84478450+dvub@users.noreply.github.com>
2023-05-31 20:29:07 -0700

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
-
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }: AppProps) {
 
 
     return (
         <>
             <Navbar />
+            <Analytics/>
             <Component {...pageProps} ></Component>
         </>
     );
