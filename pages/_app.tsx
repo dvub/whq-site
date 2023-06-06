100701cc3c8e50b171f45a4d05f434478259a975
dvub <84478450+dvub@users.noreply.github.com>
2023-06-05 17:29:56 -0700

    change style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
 import { Analytics } from '@vercel/analytics/react';
+import MusicNote from '@/components/musicnote';
 function MyApp({ Component, pageProps }: AppProps) {
 
 
     return (
         <>
             <Navbar />
-            <Analytics/>
+            <Analytics />
             <Component {...pageProps} ></Component>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <MusicNote />
+            </div>
         </>
     );
 }
