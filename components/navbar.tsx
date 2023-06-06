44e1efdcd627ecfd9069c254bbd222b73d9bbf2f
dvub <84478450+dvub@users.noreply.github.com>
2023-06-05 17:29:41 -0700

    style changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
+import Image from 'next/image'
 
 function classNames(...classes: any) {
     return classes.filter(Boolean).join(' ')
@@ -18,13 +18,17 @@ export default function Navbar() {
         { name: 'About', href: 'about', current: path === '/about' },
         { name: 'Our Work', href: 'work', current: path === '/work' },
         { name: 'Contact', href: 'contact', current: path === '/contact' },
+        { name: 'Support Us', href: 'support', current: path === '/support'}
     ]
     return (
         <Disclosure as="nav">
             {({ open }) => (
                 <div className="nav">
+
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+
                         <div className="relative flex h-16 items-center justify-between">
+
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
                                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:#8b8b8b focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -37,6 +41,9 @@ export default function Navbar() {
                                 </Disclosure.Button>
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center" style={{position: 'relative', height: '3rem', width: '3rem'}}>
+                                    <Image alt="logo" src="/logo_text.png" fill quality={100} />
+                                </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
